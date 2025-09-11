5ef3be1b1ab6acdcf9ef75a791bf72d9182ee47c
Siimesmaa <ville.siimesmaa@seamk.fi>
2025-09-11 14:30:57 +0300

    Tehtävä 1.6-1.10

--- src/App1.6-1.10.jsx
+++ src/App1.6-1.10.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
 import { useState } from 'react'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <div>{text}{value}</div>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = (good / all) * 100 || 0
+
 
   return (
-    <div>
-      code here
-    </div>
+     <div>
+      <StatisticLine text="Hyvä: " value={good} />
+      <StatisticLine text="Neutraali: " value={neutral} />
+      <StatisticLine text="Huono: " value={bad} />
+      <StatisticLine text="Yhteensä: " value={all} />
+      <StatisticLine text="Keskiarvo: " value={average.toFixed(1)} />
+      <StatisticLine text="Positiivisia: " value={positive.toFixed(1) + ' %'} />
+     </div>
   )
 }
 
-export default App
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>Anna palautetta</h1>
+      <Button handleClick={() => setGood(good + 1)} text="hyvä" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutraali" />
+      <Button handleClick={() => setBad(bad + 1)} text="huono" />
+      <h1>Tilastot</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,4 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-
-
-
-const result = notes.map(note => note.id)
-console.log(result)
-
-const result2 = notes.map(note => note.content)
-console.log(result2)
-
-const result3 = notes.map(note => {
-    '<li>' + note.content + '</li>'
-})
-console.log(result3)
-
-const result4 = notes.filter(note => note.important == true)
-console.log(result4)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
